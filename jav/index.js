6d0ee3b220f54bdad6039b2fe58e4af264be330f
ntuan16062006 <129843581+ntuan16062006@users.noreply.github.com>
2023-04-04 16:24:43 +0700

    Update and rename javascript/index.js to jav/index.js

--- jav/index.js
+++ jav/index.js
@@ -46,28 +46,28 @@ tick();
 
 function TikTok() {
     setTimeout(function() {
-            window.open('https://www.tiktok.com/@nghiepcoder16', 'ultimate')
+            window.open('https://www.tiktok.com/@tukutara16', 'ultimate')
         },
         100);
 }
 
 function Facebook() {
     setTimeout(function() {
-            window.open('https://www.facebook.com/NghiepMeow.Developer', 'ultimate')
+            window.open('https://www.facebook.com/caocuaai', 'ultimate')
         },
         100);
 }
 
 function Github() {
     setTimeout(function() {
-            window.open('https://github.com/NghiepCoder16', 'ultimate')
+            window.open('https://github.com/ntuan16062006', 'ultimate')
         },
         100);
 }
 
 function Telegram() {
     setTimeout(function() {
-            window.open('https://t.me/NghiepMeow', 'ultimate')
+            window.open('https://t.me/Ntuan16062006', 'ultimate')
         },
         100);
 }
